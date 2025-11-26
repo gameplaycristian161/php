66bc54e0bec2fc24c1705c28cfc61adf5c7b1caf
Cristian Gutierrez <gameplaycristian98@gmail.com>
2025-11-26 16:55:40 -0500

    WHERE

--- update.php
+++ update.php
@@ -1,11 +1,11 @@
 <?php
 include "conexion.php";
 
-$sql = "UPDATE productos SET nombre=:n, precio=:p WERE id=;id";
+$sql = "UPDATE productos SET nombre=:n, precio=:p WHERE id=;id";
 $stmt = $pdo->prepare($sql);
 $stmt->execute([
     'n' => $_POST['nombre'],
-    'n' => $_POST['precio'],
+    'p' => $_POST['precio'],
     'id' => $_POST['id']
 ]);
 
